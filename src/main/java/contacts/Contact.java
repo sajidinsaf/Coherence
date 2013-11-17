c3be9907ecc816a4498404d93a9502377a760cbe
sajidinsaf <sajidinsaf@yahoo.com>
2013-11-17 11:59:28 +0000

    Repo name chance

--- src/main/java/contacts/Contact.java
+++ src/main/java/contacts/Contact.java
@@ -2,4 +2,14 @@ package contacts;
 
 public class Contact {
 
+	
+	public static void main(String[] args) {
+		String[] array = new String[] {"dsf", "sf", null, "sdf", "sdf"};
+		
+		String a = null;
+		
+		for (int i=0; i<array.length || (a=array[i]) != null ; i++) {
+			System.out.println(a);
+		}
+	}
 }
